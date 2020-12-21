dea1a5f1cf9b01fa0d978204d4b5dd11e5b934d6
MuhammadHassan77 <66616106+MuhammadHassan77@users.noreply.github.com>
2020-12-21 10:28:18 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,64 +1,64 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div style={{"backgroundColor":"cyan"}}>
-        <h1 style={{ "color": "blue","bachgroundColor":"cyan" }}>Navbar</h1>
-      </div>
-    )
-  }
-}
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 style={{ "color": "red" }}>Header</h1>
-      </div>
-    )
-  }
-}
-class Footer extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Footer</h1>
-      </div>
-    )
-  }
-}
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Header />
-        <h1>Hello World</h1>
-        <Footer />
-      </div>
-    )
-  }
+import Navbar from './components/navbar.jsx';
+import Header from './components/header.jsx';
+import Main from './components/main.jsx';
+import Footer from './components/footer';
+import Post from './components/post.jsx';
+import { Switch, Route } from 'react-router-dom';
+import Attributes from './components/attributes.jsx';
+// import { set_data } from './store/action'
+// import state  from './store/reducer/index'
+// import Preview from './components/preview.jsx';
+// import Attributes from './components/attributes.jsx';
+// import Advertisement from './components/adv.jsx';
+
+class App extends Component {
+    render() {
+        // console.log(this.props)
+        return (
+
+            <Switch>
+                <React.Fragment>
+                    <div className="d-block">
+                        <div className="col-lg-12 col-sm-6 pl-0">
+                            <Navbar city={this.props.city} province={this.props.province}  categories={this.props.categories} subcategories={this.props.subcategories} />
+                        </div>
+                        <div className="col-lg-12 col-sm-6 pt-5">
+                            <Header categories={this.props.categories} subcategories={this.props.subcategories} />
+                        </div>
+                    </div>
+
+                    {/* <Main categories={this.props.categories} subcategories={this.props.subcategories} adv={this.props.adv} /> */}
+                    <Route exact path="/post" render={(props) => <Post {...props} categories={this.props.categories} subcategories={this.props.subcategories} />} />
+                    <Route path="/post/attributes" render={(props) => <Attributes {...props} categories={this.props.categories} subcategories={this.props.subcategories} />} />
+                    <Route path="/" render={(props) => <Main {...props} categories={this.props.categories} subcategories={this.props.subcategories} adv={this.props.adv} />} />
+
+                    {/* <Route path="/" component={Footer} /> */}
+
+                    <Footer />
+                </React.Fragment>
+            </Switch >
+        );
+    }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Hello World
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        province: state.province,
+        city: state.city,
+        categories: state.categories,
+        subcategories: state.subcategories,
+        adv: state.adv,
+    }
+}
+
+// const mapDispatchToProps = (dispatch) => ({
+//     set_data: (data) =>
+//         dispatch(set_data(data))
+// })
+// export default connect(mapStateToProps, mapDispatchToProps)(App2);
+
+export default connect(mapStateToProps, null)(App);
